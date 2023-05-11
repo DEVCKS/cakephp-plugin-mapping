d13f6a0242c16dcc26741bd7d2fad626a66a6ddb
ThierryCHAPEY <thierry.chapey@neqo.eu>
2023-05-11 17:17:36 +0200

    Fixed deprecrated call

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,9 @@
 <?php
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
-
-Router::plugin(
+$routes->plugin(
     'Mapping',
     ['path' => '/mapping'],
-    function (RouteBuilder $routes) {
+    function ($routes) {
         $routes->fallbacks(DashedRoute::class);
         $routes->get('/cpv', ['controller' => 'NormalizedTaxonomies', 'action' => 'indexCpv']);
         $routes->get('/nacre', ['controller' => 'NormalizedTaxonomies', 'action' => 'indexNacre']);
