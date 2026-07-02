e525a93e58cf1b9b8d250f6435e65df6944476c8
ThierryCHAPEY <thierry.chapey@neqo.eu>
2026-07-02 10:27:35 +0200

    Transform shell to command

---
+++ src/Command/MappingInstallCommand.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Command;
+
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Datasource\ConnectionManager;
+use Mapping\MappingConfig;
+
+class MappingInstallCommand extends \App\Command\BaseCommand
+{
+    public static function defaultName(): string
+    {
+        return 'mapping install';
+    }
+
+    public function execute(Arguments $args, ConsoleIo $io): ?int
+    {
+        $conf = MappingConfig::getConfig();
+        $conn = ConnectionManager::get('default');
+        $conn->transactional(function () use (&$conn, $conf, $io) {
+            foreach ($conf['entities_mapped'] as $taxo => $entities) {
+                foreach ($entities as $entity) {
+                    $tableLabel = "mapping_" . strtolower($taxo) . "_" . strtolower($entity);
+                    $taxoIdLabel = strtolower($taxo) . "_id";
+                    $entityIdLabel = substr(strtolower($entity), 0, -1) . "_id";
+
+                    try {
+                        $conn->execute('SELECT 1 FROM `' . $tableLabel . '`');
+                    } catch (\Exception $e) {
+                        try {
+                            $sql = "CREATE TABLE `" . $tableLabel . "`";
+                            $sql .= "(`" . $taxoIdLabel . "` INT(11) UNSIGNED NOT NULL";
+                            $sql .= ", `" . $entityIdLabel . "` INT(11) NOT NULL) ENGINE = InnoDB;";
+
+                            $conn->execute($sql);
+                            $conn->execute("ALTER TABLE `" . $tableLabel . "` ADD UNIQUE( `" . $taxoIdLabel . "`, `" . $entityIdLabel . "`);");
+                            $conn->execute("ALTER TABLE `" . $tableLabel . "` ADD CONSTRAINT `" . $tableLabel . "_ibfk_1` FOREIGN KEY (`" . $entityIdLabel . "`) REFERENCES `" . strtolower($entity) . "`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;");
+                        } catch (\Exception $e) {
+                            $io->error($e->getMessage());
+                            return false;
+                        }
+                    }
+
+                    $entityLabel = '';
+                    foreach (explode('_', $tableLabel) as $underscored) {
+                        $entityLabel .= ucfirst($underscored);
+                    }
+
+                    $entityTableLabel = $entityLabel . 'Table';
+                    $entityLabel = substr($entityLabel, 0, -1);
+
+                    $entityPath = dirname(dirname(__FILE__)) . "/Model/Entity/" . $entityLabel . ".php";
+                    if (!file_exists($entityPath)) {
+                        file_put_contents($entityPath, $this->getEntityFileContent($entityLabel, $taxoIdLabel, $entityIdLabel));
+                    }
+
+                    $tablePath = dirname(dirname(__FILE__)) . "/Model/Table/" . $entityTableLabel . ".php";
+                    if (!file_exists($tablePath)) {
+                        file_put_contents($tablePath, $this->getTableFileContent($entityTableLabel, $tableLabel, $taxoIdLabel, $entityIdLabel));
+                    }
+                }
+            }
+        });
+
+        $io->success('Successfully installed !');
+
+        return static::CODE_SUCCESS;
+    }
+
+    public function getEntityFileContent(string $entityLabel, string $taxoIdLabel, string $entityIdLabel): string
+    {
+        $taxoIdGetter = '';
+        $entityIdGetter = '';
+
+        foreach (explode('_', $taxoIdLabel) as $underscored) {
+            $taxoIdGetter .= ucfirst($underscored);
+        }
+
+        foreach (explode('_', $entityIdLabel) as $underscored) {
+            $entityIdGetter .= ucfirst($underscored);
+        }
+
+        return "<?php
+
+        namespace Mapping\Model\Entity;
+        
+        use Cake\ORM\Entity;
+        
+        /**
+         * @property int " . $taxoIdLabel . "
+         * @property int " . $entityIdLabel . "
+         */
+        class " . $entityLabel . " extends Entity
+        {
+            protected \$_accessible = [
+                '" . $taxoIdLabel . "' => true,
+                '" . $entityIdLabel . "' => true,
+            ];
+        
+            public function get" . $taxoIdGetter . "(): int
+            {
+                return \$this->" . $taxoIdLabel . ";
+            }
+        
+            public function get" . $entityIdGetter . "(): int
+            {
+                return \$this->" . $entityIdLabel . ";
+            }
+        }";
+    }
+
+    public function getTableFileContent(string $entityTableLabel, string $tableLabel, string $taxoIdLabel, string $entityIdLabel): string
+    {
+        return "<?php
+
+        namespace Mapping\Model\Table;
+        
+        use Cake\ORM\Table;
+        
+        class " . $entityTableLabel . " extends Table
+        {
+            use tMappingTable;
+
+            public function initialize(array \$config): void
+            {
+                parent::initialize(\$config);
+        
+                \$this->setTable('" . $tableLabel . "');
+                \$this->setPrimaryKey(['" . $taxoIdLabel . "', '" . $entityIdLabel . "']);
+
+                \$this->taxonomyIdLabel = '" . $taxoIdLabel . "';
+                \$this->entityIdLabel = '" . $entityIdLabel . "';
+            }
+        }";
+    }
+}
